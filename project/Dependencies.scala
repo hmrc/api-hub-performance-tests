34ffa34e84ea1d9292d7d9aa11ea78949f0e4138
Ade Oke <ade.oke@digital.hmrc.gov.uk>
2023-06-09 16:11:10 +0100

    wip performance test api hub applications

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,9 +5,10 @@ object Dependencies {
   private val gatlingVersion = "3.5.1"
 
   val test = Seq(
-    "com.typesafe"          % "config"                    % "1.3.1"         % Test,
-    "uk.gov.hmrc"          %% "performance-test-runner"   % "5.4.0"         % Test,
-    "io.gatling"            % "gatling-test-framework"    % gatlingVersion  % Test,
-    "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion  % Test
+    "com.typesafe"          % "config"                    % "1.3.1"        % Test,
+    "uk.gov.hmrc"          %% "performance-test-runner"   % "5.4.0"        % Test,
+    "io.gatling"            % "gatling-test-framework"    % gatlingVersion % Test,
+    "io.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersion % Test,
+    "org.apache.commons"    % "commons-text"              % "1.10.0"       % Test
   )
 }
--- src/test/scala/uk/gov/hmrc/perftests/example/ApplicationRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/example/ApplicationRequests.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.perftests.example
 
 import io.gatling.core.Predef._
@@ -7,26 +23,26 @@ import uk.gov.hmrc.performance.conf.ServicesConfiguration
 
 object ApplicationRequests extends ServicesConfiguration {
   val baseUrl: String = baseUrlFor("api-hub-applications")
-  val route: String = "/test-only/sign-in?continue_url=https%3A%2F%2Fadmin.staging.tax.service.gov.uk%2Fapi-hub"
+  val route: String = "/test-only/sign-in?continue_url=https%3A%2F%2Fadmin.qa.tax.service.gov.uk%2Fapi-hub"
   val applicationId = "646e34e1225ba0552aa72707"
   val applicationRoute = "/application-details"
 
-  val loginCredentials: Map[String, String] = Map("redirectUrl" -> "http://localhost:15018/api-hub/", "email" -> "ade.oke@digital.hmrc.gov.uk", "principal" -> "ade-service")
+  val loginCredentials: Map[String, String] = Map("redirectUrl" -> "https://admin.qa.tax.service.gov.uk/api-hub", "email" -> "ade.oke@digital.hmrc.gov.uk", "principal" -> "ade-service")
 
   val navigateToLoginPage: HttpRequestBuilder =
     http("Navigate to Api Hub Login Page")
-      .get(s"$baseUrl$route")
+      .get("https://admin.staging.tax.service.gov.uk/api-hub/test-only/sign-in?continue_url=https%3A%2F%2Fadmin.qa.tax.service.gov.uk%2Fapi-hub")
       .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
       .check(status.is(200))
 
   val loginOnLoginPage: HttpRequestBuilder =
     http("LDAP login")
-      .post(s"$baseUrl$route")
+      .post("https://admin.staging.tax.service.gov.uk//api-hub/test-only/sign-in?continue_url=https%3A%2F%2Fadmin.qa.tax.service.gov.uk%2Fapi-hub")
       .formParamMap(loginCredentials ++ Map("csrfToken" -> s"$${csrfToken}"))
       .check(status.is(303))
 
   val getApplication: HttpRequestBuilder =
     http("get specific application details")
-      .get(s"$baseUrl$applicationRoute$applicationId")
+      .get(s"https://admin.staging.tax.service.gov.uk/api-hub/application-details/$applicationId")
       .check(status.is(200))
 }
