e7c056e984042f851f54c2884209c01c35f878e2
Ade Oke <ade.oke@digital.hmrc.gov.uk>
2024-01-09 17:49:26 +0000

    updated performance tests with the new pages to be included in performance testing.

--- src/test/scala/uk/gov/hmrc/perftests/example/ApplicationSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/example/ApplicationSimulation.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,13 +17,29 @@
 package uk.gov.hmrc.perftests.example
 
 import uk.gov.hmrc.performance.simulation.PerformanceTestRunner
-import uk.gov.hmrc.perftests.example.ApplicationRequests.{getApplication, loginUsingLdap}
+import uk.gov.hmrc.perftests.example.ApplicationRequests._
 
 class ApplicationSimulation extends PerformanceTestRunner {
 
   setup("login-using-ldap", "Login to LDAP") withRequests loginUsingLdap
 
-  setup("get-application-by-id", "Get application by id") withRequests getApplication
+  setup("get-all-applications", "Get all applications") withRequests getAllApplications
+
+  setup("get-admin-access-requests", "Get all access requests") withRequests getAllAccessRequests
+
+  setup("get-application-details", "Get application details") withRequests getApplicationDetails
+
+  setup("get-application-apis", "Get application apis") withRequests getApplicationApis
+
+  setup(
+    "get-development-environment-credentials",
+    "Get development environment credentials"
+  ) withRequests getDevelopmentEnvironmentAndCredentials
+
+  setup(
+    "get-production-environment-credentials",
+    "Get production environment credentials"
+  ) withRequests getProductionEnvironmentAndCredentials
 
   runSimulation()
 }
